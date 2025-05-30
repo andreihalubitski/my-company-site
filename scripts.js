4008d554f5ce4a6753bb5cc9026b6b34dbac6684
andreihalubitski <andreihalubitski@gmail.com>
2025-05-30 15:38:23 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
     $('.project-carousel').slick({
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        arrows: true,
         dots: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.review-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 768,
